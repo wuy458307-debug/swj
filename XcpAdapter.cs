2322a15e665e28100b1e45214cf5ec98f896530a
Yan <wuy458307@gmail.com>
2026-01-22 01:10:51 +0800

    Add XCP Adapter implementation

---
+++ XcpAdapter.cs
@@ -0,0 +1,19 @@
+// XCPAdapter.cs
+
+using System;
+
+namespace YourNamespace {
+    public class XCPAdapter {
+        // Implementation of the XCP protocol goes here.
+        public void SendData(byte[] data) {
+            // Send data implementation
+        }
+
+        public byte[] ReceiveData() {
+            // Receive data implementation
+            return new byte[0];
+        }
+
+        // Other methods and properties related to the XCP protocol.
+    }
+}
